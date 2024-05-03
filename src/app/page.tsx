79d69095d572abadd59c9f1fe9b3cafa1d4b4c3d
Arjun Bhandari <er.arjunbhandari@gmail.com>
2024-05-03 19:08:01 +0545

    Database setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,6 +18,9 @@ export default async function HomePage() {
   return (
     <main>
       <div className="flex flex-wrap gap-4">
+        {posts.map(post=>(
+          <div key={post.id}>{post.name}</div>
+        ))}
       {mockImages.map((image)=>(
         <div key={image.id} className="w-48">
           <img src={image.url}/>
