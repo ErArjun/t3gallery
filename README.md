c63b1535a93764c6a904b59fcb9b006ae9e51bd3
Arjun Bhandari <er.arjunbhandari@gmail.com>
2024-05-03 18:35:19 +0545

    scaffold UI

--- README.md
+++ README.md
@@ -32,14 +32,24 @@ Follow our deployment guides for [Vercel](https://create.t3.gg/en/deployment/ver
 
 ##TODO
 
--[] Make it deploy (vercel)
--[] Scaffold basic ui with mock data
--[] Actually set up a database (vercel postgres)
--[] Attach database to ui
--[] Add authentication (w/ clerk)
--[] Add image upload
--[] Error management (w/ Santry)
--[] Routing/image page(parallel route)
--[] Delete button (w/ Server Actions)
--[] Analytics (posthog)
--[] Ratelimiting (upstash)
+-[x] Make it deploy (vercel)
+
+-[x] Scaffold basic ui with mock data
+
+-[ ] Actually set up a database (vercel postgres)
+
+-[ ] Attach database to ui
+
+-[ ] Add authentication (w/ clerk)
+
+-[ ] Add image upload
+
+-[ ] Error management (w/ Santry)
+
+-[ ] Routing/image page(parallel route)
+
+-[ ] Delete button (w/ Server Actions)
+
+-[ ] Analytics (posthog)
+
+-[ ] Ratelimiting (upstash)
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,26 @@
 import Link from "next/link";
 
+const mockUrls=[
+  'https://utfs.io/f/faf993c9-fe87-4084-8879-2f45d3a88553-i5inxt.jpg',
+  'https://utfs.io/f/1fbdcbb8-cfd0-479a-be66-6d645061bdd8-m023y9.jpg',
+  'https://utfs.io/f/ab9a2c5e-ee1f-4ba3-83e6-1233520dc850-z1vbwp.jpg',
+  'https://utfs.io/f/ab9a2c5e-ee1f-4ba3-83e6-1233520dc850-z1vbwp.jpg',
+]
+
+const mockImages=mockUrls.map((url,index)=>({
+  id:index+1,
+  url
+}))
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-     Hello (gallery in progress)
+    <main>
+      <div className="flex flex-wrap gap-4">
+      {mockImages.map(image=>(
+        <div key={image.id} className="w-48">
+          <img src={image.url}/>
+        </div>
+      ))}
+      </div>
     </main>
   );
 }
