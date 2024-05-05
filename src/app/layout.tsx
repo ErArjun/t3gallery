f0cbd34bfea791bcd96b9f63e9e4161c9b0ed70c
Arjun Bhandari <er.arjunbhandari@gmail.com>
2024-05-05 20:21:39 +0545

    change to grid

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,9 +41,13 @@ export default function RootLayout({
            */
           routerConfig={extractRouterConfig(ourFileRouter)}
         />
-      <body className={`font-sans ${inter.variable} flex flex-col gap-4`}>
+      <body className={`font-sans ${inter.variable}`}>
+        <div className="h-screen grid grid-rows-[auto,1fr]">
         <TopNav/>
+        <main className="overflow-y-scroll">
         {children}
+        </main>
+        </div>
         {modal}
         <div id="modal-root" />
         </body>
