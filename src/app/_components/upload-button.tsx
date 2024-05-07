940688c816147cd5df83ed3dcfbea736d3808ca8
Arjun Bhandari <er.arjunbhandari@gmail.com>
2024-05-07 22:58:38 +0545

    ratelimit implemented

--- src/app/_components/upload-button.tsx
+++ src/app/_components/upload-button.tsx
@@ -61,6 +61,11 @@ export function SimpleUploadButton(){
             id:"upload-begin"
           })
       },
+      onUploadError(error){
+        posthog.capture("upload-error",{error});
+        toast.dismiss('upload-begin')
+        toast.error("Upload failed")
+      },
 
         onClientUploadComplete(){
           toast.dismiss('upload-begin')
