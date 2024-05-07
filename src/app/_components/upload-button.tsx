c0bc859ca4fce114754897f2d727bf49ea348ac5
Arjun Bhandari <er.arjunbhandari@gmail.com>
2024-05-07 17:27:28 +0545

    upload event capture

--- src/app/_components/upload-button.tsx
+++ src/app/_components/upload-button.tsx
@@ -3,6 +3,7 @@
 import { useRouter } from "next/navigation";
 import { useUploadThing } from "~/utils/uploadthing";
 import {toast} from "sonner"
+import { usePostHog } from "posthog-js/react";
 
 // inferred input off useUploadThing
 type Input = Parameters<typeof useUploadThing>;
@@ -38,31 +39,25 @@ function UplaodSVG(){
 }
 
 function LoadingSpinnerSVG(){
-  return   <svg
-   width="24" 
-   height="24" 
-   stroke="#000" 
-   viewBox="0 0 24 24" 
-   xmlns="http://www.w3.org/2000/svg" 
-   fill="white"
-   ><g className="spinner_V8m1">
-    <circle cx="12" cy="12" r="9.5" fill="none" stroke-width="3"></circle>
-    </g>
-    </svg>
+  return <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="white">
+    <path d="M12,1A11,11,0,1,0,23,12,11,11,0,0,0,12,1Zm0,19a8,8,0,1,1,8-8A8,8,0,0,1,12,20Z" opacity=".25"/>
+    <path d="M10.14,1.16a11,11,0,0,0-9,8.92A1.59,1.59,0,0,0,2.46,12,1.52,1.52,0,0,0,4.11,10.7a8,8,0,0,1,6.66-6.61A1.42,1.42,0,0,0,12,2.69h0A1.57,1.57,0,0,0,10.14,1.16Z" className="spinner_ajPY"/></svg>
 }
 
 
 export function SimpleUploadButton(){
     const router=useRouter()
+    const posthog=usePostHog()
 
     const {inputProps}=useUploadThingInputProps("imageUploader",{
 
       onUploadBegin(){
+        posthog.capture('upload-begin')
         toast(
-        <div className="flex gap-2 items-center">
+        <div className="flex gap-2 items-center text-white">
           <LoadingSpinnerSVG/> <span className="text-lg">Uploading...</span> 
           </div>,{
-            duration:10000,
+            duration:50000,
             id:"upload-begin"
           })
       },
